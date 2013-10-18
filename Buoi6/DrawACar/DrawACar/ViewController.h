42d7a4e98a0e42b09626d74b35d8fe1ca4aa97c3
tranvuonganh7m <apple@lab46.vtca.vn>
2013-10-18 16:15:49 +0700

    Create a car

---
+++ Buoi6/DrawACar/DrawACar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DrawACar
+//
+//  Created by Mr Bin on 10/18/13.
+//  Copyright (c) 2013 VuongAnh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Buoi6/DrawACar/DrawACar/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  DrawACar
+//
+//  Created by Mr Bin on 10/18/13.
+//  Copyright (c) 2013 VuongAnh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)createACar:(id)sender {
+    
+    printf("         *************       \n");
+    printf("       *               *     \n");
+    printf("   ***                   ****\n");
+    printf("   * * * *            * * * *\n");
+    printf("   **  *  ************  *  **\n");
+    printf("     *   *            *    * \n");
+
+
+    
+}
+@end
