0f1c07909ac5ee5c0ece9953b85c193d102f4e57
tranvuonganh7m <apple@Apples-MacBook-Air-268.local>
2013-10-28 17:15:06 +0700

    Bai tap buoi 9

---
+++ Buoi9/MyAutoLayout/MyAutoLayout/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyAutoLayout
+//
+//  Created by Mr Bin on 10/28/13.
+//  Copyright (c) 2013 VuongAnh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
